12d4a591a575f5e7feb192a4bb2d520bcd849b8b
pfemmeni <gnadkiewicz@hotmail.com>
2021-03-09 16:20:48 +0100

    TransactionServiceTest almost completed (create_withdrawal_transaction_success - not completed)

--- src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
+++ src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
@@ -1,8 +1,11 @@
 package se.sensera.banking.impl;
 
 import se.sensera.banking.*;
+import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
+import se.sensera.banking.exceptions.UseExceptionType;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -18,20 +21,42 @@ public class TransactionServiceImpl implements TransactionService {
         this.transactionsRepository = transactionsRepository;
     }
 
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
     @Override
     public Transaction createTransaction(String created, String userId, String accountId, double amount) throws UseException {
-//       TransactionImpl transaction = new TransactionImpl(UUID.randomUUID().toString(), created,
-//               usersRepository.getEntityById(userId).get(),
-//               accountsRepository.getEntityById(accountId).get(),
-//               amount);
-//        return transactionsRepository.save(transaction);
-        return null;
+        Account account = accountsRepository.getEntityById(accountId)
+                .orElseThrow(() -> new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.ACCOUNT_NOT_FOUND));
+
+//        Transaction oldTransaction = transactionsRepository.all().(transaction -> transaction.getUser().equals(userId));
+//        if ((oldTransaction.getAmount() + amount) < 0) {
+//            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_FUNDED);
+//        }
+
+        if (account.getOwner().getId().equals(userId) || account.getUsers().anyMatch(user -> user.getId().equals(userId))) {
+            return getTransaction(created, userId, accountId, amount);
+        } else {
+            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_ALLOWED);
+        }
+    }
+
+    private Transaction getTransaction(String created, String userId, String accountId, double amount) {
+        try {
+            TransactionImpl transaction = new TransactionImpl(UUID.randomUUID().toString(), formatter.parse(created),
+                    usersRepository.getEntityById(userId).get(),
+                    accountsRepository.getEntityById(accountId).get(),
+                    amount);
+            return transactionsRepository.save(transaction);
+        } catch (ParseException e) {
+            throw new RuntimeException("Fail in createTransaction because parse failed " + created);
+        }
     }
 
     @Override
     public double sum(String created, String userId, String accountId) throws UseException {
         return 0;
     }
+
 
     @Override
     public void addMonitor(Consumer<Transaction> monitor) {
