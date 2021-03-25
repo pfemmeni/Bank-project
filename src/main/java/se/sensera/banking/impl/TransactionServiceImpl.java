5c5db85ed5faf01054e70f846ac993eb466b55fc
pfemmeni <gnadkiewicz@hotmail.com>
2021-03-25 18:29:08 +0100

    Cleaner code in TransactionServiceImpl and TransactionServiceParallelTest completed!

--- src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
+++ src/main/java/se/sensera/banking/impl/TransactionServiceImpl.java
@@ -5,13 +5,12 @@ import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
 import se.sensera.banking.exceptions.UseExceptionType;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TransactionServiceImpl implements TransactionService {
@@ -26,36 +25,29 @@ public class TransactionServiceImpl implements TransactionService {
         this.transactionsRepository = transactionsRepository;
     }
 
+    static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     @Override
     public Transaction createTransaction(String created, String userId, String accountId, double amount) throws UseException {
+        Date date = getDate(created);
         Account account = getAccountById(accountId)
                 .orElseThrow(() -> new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.ACCOUNT_NOT_FOUND));
 
-        if (!isUserOrOwner(userId, account)) {
+        if (!isUserOrOwner(userId, account))
             throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_ALLOWED);
-        }
-        Date date = getDate(created);
+
+        double sum = sum(date, userId, account);
 
         synchronized (this) {
-            if (sum(date, userId, account) + amount < 0) {
+            if (sum + amount < 0)
                 throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_FUNDED);
-            }
         }
         return createNewTransaction(date, userId, accountId, amount);
-
     }
 
-    private Optional<Account> getAccountById(String accountId) {
-        return accountsRepository.getEntityById(accountId);
-    }
-
-    private Date getDate(String created) {
-        return stringToDate(created);
-    }
 
     private Transaction createNewTransaction(Date created, String userId, String accountId, double amount) {
-        TransactionImpl transaction = new TransactionImpl(
-                UUID.randomUUID().toString(),
+        TransactionImpl transaction = new TransactionImpl(UUID.randomUUID().toString(),
                 created,
                 usersRepository.getEntityById(userId).get(),
                 getAccountById(accountId).get(),
@@ -80,16 +72,13 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     private double sum(Date created, String userId, Account account) throws UseException {
-        if (isUserOrOwner(userId, account)) {
+        if (isUserOrOwner(userId, account))
             return sumOfFoundTransactions(created, account);
-        } else {
-            throw new UseException(Activity.SUM_TRANSACTION, UseExceptionType.NOT_ALLOWED);
-        }
+        throw new UseException(Activity.SUM_TRANSACTION, UseExceptionType.NOT_ALLOWED);
     }
 
     private double sumOfFoundTransactions(Date created, Account account) {
-        Stream<Transaction> foundTransactions = getTransactions(created, account);
-        return foundTransactions.mapToDouble(Transaction::getAmount).sum();
+        return getTransactions(created, account).mapToDouble(Transaction::getAmount).sum();
     }
 
 
@@ -99,29 +88,31 @@ public class TransactionServiceImpl implements TransactionService {
                         && t.getCreated().before(created));
     }
 
-    private Boolean isUserOrOwner(String userId, Account account) {
-        if (account.getOwner().getId().equals(userId)) {
-            return true;
-        }
-        if (account.getUsers().anyMatch(u -> u.getId().equals(userId))) {
-            return true;
-        }
-        return false;
+    private Optional<Account> getAccountById(String accountId) {
+        return accountsRepository.all().filter(account -> accountId.equals(accountId))
+                .collect(Collectors.toList())
+                .stream()
+                .findAny();
     }
 
-    static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private Boolean isUserOrOwner(String userId, Account account) {
+        return account.getOwner().getId().equals(userId)
+                || (account.getUsers().anyMatch(u -> u.getId().equals(userId)));
+    }
+
+    private Date getDate(String created) {
+        return stringToDate(created);
+    }
 
     private Date stringToDate(String created) {
         LocalDateTime localDateTime = LocalDateTime.parse(created, formatter2);
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-
     }
-
 
     @Override
     public void addMonitor(Consumer<Transaction> monitor) {
         transactionListeners.add(monitor);
-
     }
 }
 
